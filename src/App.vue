865c139beed0ccb1691f37d6ad3d1a76d8c6a6c7
Grin82 <grin82@gmail.com>
2019-04-07 13:31:27 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <testComponent/>
+  </div>
+</template>
+
+<script>
+import testComponent from './components/testComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    testComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
